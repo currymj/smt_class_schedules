48c11b115482e78104336e45614c4257d7364227
Michael J. Curry <curry@cs.umd.edu>
2018-08-18 12:30:27 -0400

    remove tests from other file

--- scheduler.py
+++ scheduler.py
@@ -89,37 +89,3 @@ def make_constraints(students, sections):
     # what else might we need????
     return s
 
-def test_one():
-    students = {
-        "alice": {
-            "classes":[[13.5, 14.5], [9.5,10.75]],
-            "preferences": []
-        },
-        "bob": {"classes": [[9.5, 11.0]], "preferences": []},
-        "charlie": {"classes": [], "preferences": []}
-    }
-
-    sections = [Section('1111', [8.0, 9.0]), Section('2222', [9.0, 10.0]), Section('3333', [11.0, 12.0]), Section('4444', [13.0, 14.0])]
-
-    s = make_constraints(students, sections)
-    assert(s.check().__repr__ == 'sat')
-
-def test_overlap_sections():
-    students = {"charlie": {"classes": [], "preferences": []}}
-    sections = [Section("1111", [9.0,10.0]), Section("2222", [9.0,10.0])]
-    s = make_constraints(students, sections)
-    assert(s.check().__repr__ == 'unsat')
-
-def test_backtoback_different():
-    students = {"charlie": {"classes": [], "preferences": []}}
-    sections = [Section("1111", [9.0,10.0]), Section("2222", [10.0,11.0])]
-    s = make_constraints(students, sections)
-    assert(s.check().__repr__ == 'unsat')
-
-
-def test_backtoback_same():
-    students = {"charlie": {"classes": [], "preferences": []}}
-    sections = [Section("1111", [9.0,10.0]), Section("1111", [10.0,11.0])]
-    s = make_constraints(students, sections)
-    assert(s.check().__repr__ == 'sat')
-
